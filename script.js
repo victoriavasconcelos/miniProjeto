881df411ca4605a72eb274821db6e57899542eca
victoriavasconcelos <victoriabarbosa618@gmail.com>
2023-09-27 21:47:31 -0300

    inclusão do botão de editar

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ class Usuario {
 
 const usuarios = []
 
+var indiceAtual = -1
+
 function adicionarUsuario (){
     const nome = document.getElementById("campoNome").value
     const email = document.getElementById("campoEmail").value
@@ -22,7 +24,7 @@ function adicionarUsuario (){
     }else{
         alert("Preencha os campos")
     }
-    console.log(usuarios)
+    
 }
 
 function deletarCadastro (index){
@@ -44,18 +46,83 @@ function mostrarCadastros (){
         return
     }
     let lista = "<ul>"
-    usuariosCadastrados.forEach((usuario,index)=>{
-        lista += `<li> ${usuario.nome} ${usuario.email} <button onclick="deletarCadastro(${index})">Deletar</button> <button onclick="editar(${index})">Editar</button></li>`
-    
-    })
+    usuariosCadastrados.forEach((usuario, index) => {
+        lista += `<li> ${usuario.nome} ${usuario.email} 
+        <button onclick="deletarCadastro(${index})">Deletar</button>
+        <button id = "botaoEditar" onclick ="editarUsuario(${index}, ${index})">Editar</button></li>`;
+    });
     lista += "</ul>"
     document.getElementById("mostrarUsers").innerHTML = lista
 }
 
-function editar(){
-    const usuariosCadastrados = JSON.parse(cadastros)
-    const 
+function editarUsuario(index) {
+    const nomeEdit = document.getElementById("campoNomeEdit");
+    const labelNome = document.getElementById("labelCampoNome");
+    const emailEdit = document.getElementById("campoEmailEdit");
+    const labelEmail = document.getElementById("labelCampoEmail");
+    const senhaEdit = document.getElementById("campoSenhaEdit");
+    const labelSenha = document.getElementById("labelCampoSenha");
+    const botaoEditar = document.getElementById("botaoEditar");
+    const botaoSalvar = document.getElementById("botaoSalvar");
+
+    indiceAtual = index;
+
+    const styleDisplay = nomeEdit.style.display;
+    if (styleDisplay === "none" || styleDisplay === "") {
+        nomeEdit.style.display = "inline-block";
+        labelNome.style.display = "inline-block";
+        emailEdit.style.display = "inline-block";
+        labelEmail.style.display = "inline-block";
+        senhaEdit.style.display = "inline-block";
+        labelSenha.style.display = "inline-block";
+        botaoEditar.style.display = "none";
+        botaoSalvar.style.display = "inline-block";
+
+        const usuariosCadastrados = JSON.parse(localStorage.getItem("cadastros"));
+        const usuario = usuariosCadastrados[index];
+        nomeEdit.value = usuario.nome;
+        emailEdit.value = usuario.email;
+        senhaEdit.value = usuario.senha;
+    } else {
+        nomeEdit.style.display = "none";
+        labelNome.style.display = "none"
+        emailEdit.style.display = "none";
+        labelEmail.style.display = "none";
+        senhaEdit.style.display = "none";
+        labelSenha.style.display = "none";
+        botaoEditar.style.display = "inline-block";
+        botaoSalvar.style.display = "none";
+    }
 }
+
+function salvarEdicao() {
+    const nomeEdit = document.getElementById("campoNomeEdit").value;
+    const emailEdit = document.getElementById("campoEmailEdit").value;
+    const senhaEdit = document.getElementById("campoSenhaEdit").value;
+    console.log(indiceAtual)
+    if (nomeEdit && emailEdit && senhaEdit) {
+        const usuariosCadastrados = JSON.parse(localStorage.getItem("cadastros"));
+        
+        if (usuariosCadastrados) {
+            const usuarioEditado = new Usuario(nomeEdit, emailEdit, senhaEdit);
+            usuariosCadastrados[indiceAtual] = usuarioEditado;
+            
+            localStorage.setItem("cadastros", JSON.stringify(usuariosCadastrados));
+
+            mostrarCadastros();
+            
+            document.getElementById("campoNomeEdit").value = "";
+            document.getElementById("campoEmailEdit").value = "";
+            document.getElementById("campoSenhaEdit").value = "";
+            console.log(history.state)
+            history.back();
+        }
+    } else {
+        alert("Preencha todos os campos para salvar a edição.");
+    }
+}
+
+
 
 document.addEventListener("DOMContentLoaded", mostrarCadastros)
 
